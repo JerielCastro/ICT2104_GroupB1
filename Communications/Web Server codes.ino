66e89c6b9707d38a838a465a819ec9732f8bc653
Jeriel Castro <89906842+JerielCastro@users.noreply.github.com>
2022-11-24 17:37:32 +0800

    Update Web Server codes.ino

--- Communications/Web Server codes.ino
+++ Communications/Web Server codes.ino
@@ -9,7 +9,6 @@
 #include <HardwareSerial.h>
 
 // define pins
-#define output10 10
 #define receiver 33
 #define transmitter 32
 
@@ -36,16 +35,9 @@ const long timeoutTime = 2000;
 void setup() {
   Serial.begin(115200);
 
-  // Initialize G10(LED) as OUTPUT
-  pinMode(output10, OUTPUT);
-
   // Init transmitter as output and receiver as input
   pinMode(receiver, INPUT);
   pinMode(transmitter, OUTPUT);
-
-  // Set G10 to LOW
-  digitalWrite(output10, LOW);
-
 
   // Connect to Wi-Fi network with SSID and password
   Serial.print("Connecting to ");
@@ -94,17 +86,6 @@ void loop() {
             client.println("Connection: close");
             client.println();
 
-            // turns the GPIOs on and off
-            if (header.indexOf("GET /10/on") >= 0) {
-              Serial.println("GPIO 10 on");
-              output10State = "on";
-              digitalWrite(output10, HIGH);
-            } else if (header.indexOf("GET /10/off") >= 0) {
-              Serial.println("GPIO 10 off");
-              output10State = "off";
-              digitalWrite(output10, LOW);
-            }
-
             // Display the HTML web page
             client.println("<!DOCTYPE html><html>");
             client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
@@ -127,31 +108,15 @@ void loop() {
             // Web Page Heading
             client.println("<body><h1>test Web Server</h1>");
 
-            // Display current state, and ON/OFF buttons for GPIO 26
-            client.println("<p>GPIO 10 - State " + output10State + "</p>");
-            // If the output26State is off, it displays the ON button
-            if (output10State == "off") {
-              client.println("<p><a href=\"/10/on\"><button class=\"button\">ON</button></a></p>");
-            } else {
-              client.println("<p><a href=\"/10/off\"><button class=\"button button2\">OFF</button></a></p>");
-            }
-
             //To Show Data
-            //characters 
-            //Speed
+            //Speed of car
             double temp = analogRead(receiver);
             String speed = String(temp);
-            client.println("<p class=\"par\">test </p>");
-            // if ( temp < 21)
-            client.println("<p> <span class=\"inpu\" style=\"background-color: #198754!important; color:#f8f9fa!important;\"> " + speed + " </span></p>");
-            // else if ( temp > 24)
-            //   client.println("<p> <span class=\"lm35\" style=\"background-color: #dc3545!important; color:#f8f9fa!important;\"> " + celsius + "°C </span></p>");
-            // else
-            //   client.println("<p> <span class=\"lm35\" style=\"background-color: #ffc107!important; color:#212529!important;\"> " + celsius + "°C </span></p>");
-
-            // String test = analogRead(receiver);
-            // client.println("<p class=\"par\">testing</p>");
-            // client.println("<p class=\"par\"> test </p>");
+            client.println("<p class=\"par\"> OUTPUT DATA </p>");
+            client.println("<p> <span class=\"speed\" style=\"background-color: #198754!important; color:#f8f9fa!important;\"> " + speed + " </span></p>");
+        
+            //characters from barcode
+            //client.println("<p> <span class=\"characters\" style=\"background-color: #198754!important; color:#f8f9fa!important;\"> " + speed + " </span></p>");
             
             client.println("</body></html>");
 
